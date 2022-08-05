9608405a08dd1bc4ec2379ecaaf0c09e2fc3521b
Nesterenko-Artem <petr79petrov1979@gmail.com>
2022-08-05 23:46:29 +0300

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox fromlightbox.css';
+// import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+
 // Change code below this line
 
 console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const cardsMarkup = renderCardGalery(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', cardsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+lightbox.on('show.simplelightbox');
+
+function renderCardGalery(elements) {
+  return elements
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" 
+      alt="${description}" />
+      </a>
+      `;
+    })
+    .join('');
+}
