efe2bc2d9308aab1b28602dcb071e4680d2b4318
Nesterenko-Artem <petr79petrov1979@gmail.com>
2022-08-06 09:07:08 +0300

    fix gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
-import SimpleLightbox fromlightbox.css';
-// import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
-
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
