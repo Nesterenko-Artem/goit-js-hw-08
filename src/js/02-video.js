866f9cb43d9de917aa497fd93d9881f62ae709b4
Nesterenko-Artem <petr79petrov1979@gmail.com>
2022-08-06 15:45:51 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,30 @@
 import Player from '@vimeo/player';
 const throttle = require('lodash.throttle');
 
-const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
-console.log(iframe);
-// console.log(Player);
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
 
-// player.on('play', function () {
-//   console.log('Played the video');
-// });
 
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
+
+let saveTime = localStorage.getItem(CURRENT_TIME);
+let storageTime = checkStorageTime(saveTime);
+
+player.setCurrentTime(storageTime).then(function (seconds) {});
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function checkStorageTime(time) {
+  try {
+    return time ? JSON.parse(time) : 0;
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
+
+function saveCurrentTime() {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem(CURRENT_TIME, JSON.stringify(seconds));
+  });
+}
