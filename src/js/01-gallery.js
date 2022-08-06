2db765d3be5d49cf797258ba5170780925810109
Nesterenko-Artem <petr79petrov1979@gmail.com>
2022-08-06 10:22:16 +0300

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,7 @@
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
+// ==========================
 
 console.log(galleryItems);
 const galleryRef = document.querySelector('.gallery');
