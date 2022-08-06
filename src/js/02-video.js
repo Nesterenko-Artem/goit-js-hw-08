73390030ee3c6933cdd0c8981e90cca04a4f008a
Nesterenko-Artem <petr79petrov1979@gmail.com>
2022-08-06 13:10:55 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+console.log(iframe);
+// console.log(Player);
+
+// player.on('play', function () {
+//   console.log('Played the video');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
